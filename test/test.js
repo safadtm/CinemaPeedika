fc309eb56b5462bbda57fec03bcfcb52c1071305
safadtm <safadtm2004@gmail.com>
2023-11-07 10:37:38 +0530

    images updated

--- test/test.js
+++ test/test.js
@@ -1,79 +1,33 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+// script.js
+// Fetch movie data from movies.json
+fetch("movies.json")
+    .then(response => response.json())
+    .then(movieData => {
+        // Call the function to populate movie details
+        populateMovieDetails(movieData);
+    })
+    .catch(error => {
+        console.error("Error fetching movie data:", error);
+    });
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    validateInputs();
-});
-
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
+// Function to populate movie details
+function populateMovieDetails(movieData) {
+    document.getElementById("movieTitle").textContent = movieData.title;
+    document.getElementById("movieRating").textContent = movieData.rating;
+    document.getElementById("moviePoster").src = movieData.posterURL;
+    document.getElementById("movieTrailer").src = movieData.trailerURL;
+    var genreButtons = document.getElementById("movieGenres");
+    movieData.genre.forEach(function(genre) {
+        var button = document.createElement("button");
+        button.className = "transparent-button";
+        button.textContent = genre;
+        genreButtons.appendChild(button);
+    });
+    document.getElementById("movieDescription").textContent = movieData.description;
+    document.getElementById("movieDirector").textContent = movieData.director;
+    document.getElementById("movieWriters").textContent = movieData.writers.join(" • ");
+    document.getElementById("movieActors").textContent = movieData.actors.join(" • ");
+    var productionCompanyInfo = document.getElementById("productionCompanyInfo");
+    productionCompanyInfo.href = movieData.details[0].productionCompanyInfo;
 }
 
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
-
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-
-    if (usernameValue === '') {
-        setError(username, 'Username is required');
-    } else {
-        setSuccess(username);
-    }
-
-    if (emailValue === '') {
-        setError(email, 'Email is required');
-    } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
-    } else {
-        setSuccess(email);
-    }
-
-    if (passwordValue === '') {
-        setError(password, 'Password is required');
-    } else if (passwordValue.length < 8) {
-        setError(password, 'Password must be at least 8 character.')
-    } else {
-        setSuccess(password);
-    }
-
-    if (password2Value === '') {
-        setError(password2, 'Please confirm your password');
-    } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
-    } else {
-        setSuccess(password2);
-    }
-
-
-    if (document.querySelectorAll('.success').length === 4) {
-        // submit the form
-        form.submit();
-    }
-
-
-};
